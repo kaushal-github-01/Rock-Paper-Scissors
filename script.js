3ae90edefc264bb51445b2d5a272c0f07d951248
Kaushal <kaushalkundu5683@gmail.com>
2023-03-31 10:46:50 +0530

    modified overall game

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const btns = document.querySelectorAll(".btn");
+const reset = document.querySelector("#reset");
+const displayResults = document.querySelector("#displayResults");
+const displayFinalResult = document.querySelector('#displayFinalResult');
+
 getComputerChoice = () => {
-    const Choices = ['rock', 'paper', 'scissors'];
+    const Choices = ['rock', 'paper', 'scissor'];
     // var item = items[Math.floor(Math.random()*items.length)];
     let randomChoice = Choices[Math.floor(Math.random() * Choices.length)];
     return randomChoice;
-}
+};
 
 playingRound = (playerSelection, computerSelection) => {
     if (playerSelection == computerSelection) {
@@ -15,7 +20,7 @@ playingRound = (playerSelection, computerSelection) => {
             return ["You lose! Paper beats Rock", -1];
         }
         else {
-            return ["You win! Rock beats Scissors", 1];
+            return ["You win! Rock beats Scissor", 1];
         }
     }
     else if (playerSelection == "paper") {
@@ -24,57 +29,60 @@ playingRound = (playerSelection, computerSelection) => {
 
         }
         else {
-            return ["You lose! Scissors beats Paper", -1];
+            return ["You lose! Scissor beats Paper", -1];
         }
     }
-    else if (playerSelection == "scissors") {
+    else if (playerSelection == "scissor") {
         if (computerSelection == "rock") {
-            return ["You lose! Rock beats Scissors", -1];
+            return ["You lose! Rock beats Scissor", -1];
         }
         else {
-            return ["You win! Scissors beats Paper", 1];
+            return ["You win! Scissor beats Paper", 1];
         }
     }
     else {
         alert("You lost a chance, Please enter a valid choice");
     }
-}
+};
 
-game = () => {
+let count = 0;
+let finalResult = 0;
 
-    let score = 0;
+btns.forEach(function (e) {
+    e.addEventListener('click', () => {
+        count++;
+        // console.log(count);
 
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("What is your choice ?").toLocaleLowerCase();
-        let obj = playingRound(playerChoice, getComputerChoice());
-        console.log(obj[0]);
-        console.log(obj[1]);
-
-        if (obj[1] == 0) {
-            score += 0;
+        if (count < 6) {
+            let result = playingRound(e.value, getComputerChoice());
+            displayResults.innerHTML += `${result[0]} <br><br> ${result[1]}<br><br>`;
+            finalResult += result[1];
         }
-        else if (obj[1] == -1) {
-            score -= 1;
+
+        else if (count == 6) {
+            // console.clear();
+            displayResults.textContent = "";
+
+            // console.log("Gave Over");
+            // console.log("Your Final Score is:", finalResult);
+            displayFinalResult.textContent = `Your Final Score is: ${finalResult}`;
         }
-        else if (obj[1] == 1) {
-            score += 1;
+
+        else if (count > 6) {
+            displayFinalResult.textContent = "";
+            displayResults.textContent = "Please Tap on 'Crear' button";
         }
+
         else {
-            alert("Error in score");
+            console.log("Error in the Game");
         }
-    }
+    })
+});
 
-    console.log("Your final score is:", score);
-
-    if (score == 0) {
-        console.log("It's a draw in the end!");
-    }
-    else if (score > 0) {
-        console.log("You  won the overall game!");
-    }
-    else if (score < 0) {
-        console.log("You lost the overall game!");
-    }
-}
-
-game()
+reset.onclick = () => {
+    // console.clear();
+    displayResults.textContent = "";
+    displayFinalResult.textContent = "";
+    count = 0;
+    finalResult = 0;
+};
